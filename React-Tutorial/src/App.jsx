b65831e5b44dcb89ac84644ce056a144b7178c93
Olivia <larsonog@hendrix.edu>
2023-11-30 10:43:17 -0600

    Fixed a vital first step
    remember npm create vite@latest vite-tw

--- React-Tutorial/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-
-function App() {
-    return(
-    <h1>My City Encyclopedia</h1>
-    )
-}
-export default App
---
+++ vite-tw/src/App.jsx
@@ -0,0 +1,20 @@
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+//import './App.css';
+import NavBar from './components/header/Navbar';
+
+
+
+function App() {
+
+  return (
+
+      <div>
+          <NavBar />
+
+      </div>
+  
+  )
+}
+
+export default App
